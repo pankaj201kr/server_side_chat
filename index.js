f1eceb6d2c8f3831ce74fbf9e11a4edd91f8e7d0
pankaj2speqto <pankaj.kumar@speqto.com>
2023-10-21 14:44:05 +0530

    add

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:3000', 'http://51.20.187.2:3000/'],
+        origin: 'http://51.20.187.2:3000',
         methods: ['GET', 'POST', "PATCH"],
     },
 });
