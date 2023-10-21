ad5034d3bf4f3284f7d7f161132cfa26eba45e5a
pankaj2speqto <pankaj.kumar@speqto.com>
2023-10-21 11:56:57 +0530

    aad

---
+++ index.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:3000', 'http://51.20.187.2:3000/'],
+        methods: ['GET', 'POST', "PATCH"],
+    },
+});
+
+const CHAT_BOT = 'ChatBot';
+let chatRoom = '';
+let allUsers = [];
+function leaveRoom(userID, chatRoomUsers) {
+    return chatRoomUsers.filter((user) => user.id != userID);
+}
+io.on('connection', (socket) => {
+    console.log(`User connected ${socket.id}`);
+
+
+    socket.on('join_room', (data) => {
+        const { username, room } = data;
+        socket.join(room);
+
+        let __createdtime__ = Date.now();
+        socket.to(room).emit('receive_message', {
+            message: `${username} has joined the chat room`,
+            username: CHAT_BOT,
+            __createdtime__,
+        });
+        socket.emit('receive_message', {
+            message: `Welcome ${username}`,
+            username: CHAT_BOT,
+            __createdtime__,
+        });
+        chatRoom = room;
+        allUsers.push({ id: socket.id, username, room });
+        chatRoomUsers = allUsers.filter((user) => user.room === room);
+        socket.to(room).emit('chatroom_users', chatRoomUsers);
+        socket.emit('chatroom_users', chatRoomUsers);
+
+
+
+
+        socket.on('leave_room', (data) => {
+            const { username, room } = data;
+            socket.leave(room);
+            const __createdtime__ = Date.now();
+            allUsers = leaveRoom(socket.id, allUsers);
+            socket.to(room).emit('chatroom_users', allUsers);
+            socket.to(room).emit('receive_message', {
+                username: CHAT_BOT,
+                message: `${username} has left the chat`,
+                __createdtime__,
+            });
+            console.log(`${username} has left the chat`);
+        });
+    });
+
+    socket.on('send_message', (data) => {
+        const { message, username, room, __createdtime__ } = data;
+        io.in(room).emit('receive_messages', data);
+
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected from the chat');
+        const user = allUsers.find((user) => user.id == socket.id);
+        if (user?.username) {
+            allUsers = leaveRoom(socket.id, allUsers);
+            socket.to(chatRoom).emit('chatroom_users', allUsers);
+            socket.to(chatRoom).emit('receive_message', {
+                message: `${user.username} has disconnected from the chat.`,
+            });
+        }
+    });
+
+});
+
+
+app.get('/', (req, res) => {
+    res.send('Chat Application');
+});
+
+server.listen(4000, () => console.log('Server is running on port 4000'));
